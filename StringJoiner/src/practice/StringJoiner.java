a8bcacfe5880319f4f225619efda560e051d6fa8
Durgasai <139207890+durgasaipetla@users.noreply.github.com>
2024-09-17 12:51:59 +0530

    Add files via upload

---
+++ StringJoiner/src/practice/StringJoiner.java
@@ -0,0 +1,14 @@
+package practice;
+
+public class StringJoiner {
+
+	public static void main(String[] args) {
+	String str1="hello";
+	String str2="world";
+	String res=joinStrings(str1,str2);
+	System.out.println(res);
+	}
+	public static String joinStrings(String str1, String str2) {
+    return str1+str2;
+}
+}
