81d4e6c985bd6bb3947108daf094fd14704295e6
Durgasai <139207890+durgasaipetla@users.noreply.github.com>
2024-09-17 12:52:41 +0530

    Add files via upload

---
+++ TheCubeMystery/src/practise/CubeNumber.java
@@ -0,0 +1,14 @@
+package practise;
+
+import java.util.Scanner;
+
+public class CubeNumber {
+	public static int cubeNumber(int num) {
+		return num*num*num;
+	}
+	public static void main(String[] args) {
+		Scanner scan =new Scanner(System.in);
+		int num= scan.nextInt();
+		System.out.println(cubeNumber(num));
+	}
+}
