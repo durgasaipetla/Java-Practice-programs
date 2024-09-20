150ccf743cb91dbe7d7d2c28f1d98525a621b5b2
Durgasai <139207890+durgasaipetla@users.noreply.github.com>
2024-09-20 20:43:02 +0530

    Add files via upload

---
+++ TheHeightConverter/src/HeightConverterApp.java
@@ -0,0 +1,12 @@
+
+public class HeightConverterApp {
+
+	public static void main(String[] args) {
+		HeightConverter converter = new HeightConverter();
+
+		System.out.printf("%.2f",converter.convertInchesToFeet(72.0));
+		
+
+	}
+
+}
